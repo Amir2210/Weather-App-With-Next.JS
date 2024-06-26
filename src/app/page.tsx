77aa60cdb5224bd9ba5cbb36b31f3d63fdac97aa
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-26 16:55:02 +0300

    Create container cmp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,13 @@ export default async function Home() {
   return (
     <main className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
       <Navbar />
-      <section className='px-3 max-w-7xl mx-auto flex flex-col gap-9 pb-10 pt-4'>
+      <section className='px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4'>
         <section>
           <div className='flex items-center gap-2'>
-            <h2 className='flex gap-1 text-2xl items-center'>{timestampToDay(date)}</h2>
-            <p>({timestampToDate(date)})</p>
+            <h2 className='flex gap-1 text-2xl items-end'>
+              <p>{timestampToDay(date)}</p>
+              <p className='text-lg'>({timestampToDate(date)})</p>
+            </h2>
           </div>
         </section>
         <div></div>
