9964f874713bdcf325d204b09f1d81dbfa7c5d78
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-26 10:49:00 +0300

    Style the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main>
-      <h1>hello</h1>
+    <main className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
+      <Navbar />
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
 type Props = {}
 
-const Navbar = (props: Props) => {
+export default function Navbar({ }: Props) {
   return (
-    <div>Navbar</div>
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+      <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+        <div className='flex items-center justify-center gap-2'>
+          <h2 className='text-gray-500 text-3xl capitalize'>weather</h2>
+          <MdWbSunny className='text-3xl mt-1 text-yellow-300' />
+        </div>
+        <section className='flex gap-2 items-center'>
+          <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer duration-150' />
+          <MdOutlineLocationOn className='text-3xl' />
+          <p className='text-slate-900/80 text-sm capitalize'>tel aviv</p>
+          <div>
+
+          </div>
+        </section>
+      </div>
+    </nav>
   )
 }
