a2ffd18a5e9eb0509f57354750b9def3c67ea0dd
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-26 13:24:28 +0300

    Add Loading when no data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import Navbar from '@/components/Navbar'
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
 import axios from 'axios';
 import WeatherData from '@/types/Weather';
 
@@ -15,18 +10,14 @@ export default async function Home() {
   const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=tel%20aviv&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=2`)
   const weatherData: WeatherData = data
   console.log('weatherData:', weatherData)
-  // const { isPending, error, data } = useQuery<WeatherData>({
-  //   queryKey: ['repoData'],
-  //   queryFn: async () => {
-  //     const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=tel%20aviv&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=2`)
-  //     return data
-  //   }
-  // })
-  // console.log(data)
 
-  // if (isPending) return 'Loading...'
-
-  // if (error) return 'An error has occurred: ' + error.message
+  if (!data) {
+    return (
+      <div className='flex items-center min-h-screen justify-center'>
+        <p className='animate-bounce'>Loading...</p>
+      </div>
+    )
+  }
   return (
     <main className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
       <Navbar />
