cfc0cdf7001caba6f74ac7010fe505f570e58dca
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-07-03 10:19:43 +0300

    Add z index to suggestions box

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -107,7 +107,7 @@ function SuggestionsBox({ showSuggestions, suggestions, handleSuggestionClick, e
   return (
     <>
       {((showSuggestions && suggestions.length > 1) || error) && (
-        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+        <ul className="z-40 mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
           {error && suggestions.length < 1 && (
             <li className="text-red-500 p-1 "> {error}</li>
           )}
